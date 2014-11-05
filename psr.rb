44097fd077b2e277a05b1b58d9fa99333db45216
Julian Gommans <julian.s.gommans@gmail.com>
2014-11-05 17:20:06 +1300

    Update psr.rb

--- psr.rb
+++ psr.rb
@@ -1,23 +1,35 @@
-psr = ["paper",
-"scissors",
-"rock"]
+psr = [:paper,
+:scissors,
+:rock,
+:stop]
+
+RULES = {
+    :paper => {:paper => :draw, :scissors => :lose, :rock => :win},
+    :scissors => {:paper => :win, :scissors => :draw, :rock => :lose},
+    :rock => {:paper => :lose, :scissors => :win, :rock => :draw}
+}
+
+def game(x,y)
+    RULES[x][y]
+end
 
 user_psr = ""
 
-psr_choose = Proc.new {psr.each do |a|
+psr_choose = Proc.new {
+opponent = [:paper,:scissors,:rock].sample
+outcome = game(user_psr,opponent)
+
+psr.each do |a|
     if user_psr == a
-        puts "You chose #{a.capitalize}"
-        puts "You're opponent chose #{psr.sample.capitalize}."
+        puts "You chose #{user_psr.to_s.capitalize}. You're opponent chose #{opponent.to_s.capitalize}."
+    puts "You #{outcome.to_s.capitalize}!"
 end
 end
-if
-    user_psr == "stop"
-    break 
-end}
+}
 
 
 loop {   
 puts "We're playing paper, scissors, rock, choose one or 'stop' to stop"
-user_psr = gets.chomp.downcase
-
-psr_choose.call}
+user_psr = gets.chomp.downcase.to_sym
+psr_choose.call
+}
